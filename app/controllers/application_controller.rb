46800cb9fe085699dc4a6fa8ceffbf7230593401
nudrob <nudrob557@gmail.com>
2022-04-03 12:04:27 +0300

    fix users sign_in and log_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    current_user
+    super
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    request.referrer
+    super
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions'
+  }
+
   root 'home#index'
   get '/help', to: 'home#help'
   get '/about', to: 'home#about'
